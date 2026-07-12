434971473dfe5e3e7277074e77f8f5d78e157fe6
Shalvi Khare <shalvi.khare2007@gmail.com>
2026-07-13 01:57:46 +0530

    Bugs fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  outputFileTracingIncludes: {
+    '/api/birth-chart': ['./node_modules/geo-tz/data/**/*'],
+  },
 };
 
 export default nextConfig;
